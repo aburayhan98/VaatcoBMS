91f1f1615a5a879b9c8fdcdc4acdded6b513ef1b
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-07 00:31:11 +0600

    added DbConfig

--- VaatcoBMS.Application/Configs/DbConfig.cs
+++ VaatcoBMS.Application/Configs/DbConfig.cs
@@ -1,6 +1,11 @@
 ﻿
+using Microsoft.Data.SqlClient;
+
 namespace VaatcoBMS.Application.Configs;
 
 public class DbConfig
 {
+	public string ConnectionString { get; set; }
+
+	public SqlConnection Connection => new(ConnectionString);
 }
