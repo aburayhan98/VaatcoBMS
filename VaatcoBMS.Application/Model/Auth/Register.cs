4b474e13acde4dc69f946d2ca1cc05bfe83bf652
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-07 02:31:48 +0600

    added Register model

---
+++ VaatcoBMS.Application/Model/Auth/Register.cs
@@ -0,0 +1,11 @@
+﻿using VaatcoBMS.Domain.Enums;
+
+namespace VaatcoBMS.Application.Model.Auth;
+//This is DTO class
+public class Register
+{
+	public string Name { get; set; } = ""; 
+	public string Email { get; set; } = ""; 
+	public string Password { get; set; } = ""; 
+	public UserRole Role { get; set; }
+}
