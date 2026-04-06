0fe1dcda89b963b293e7ed1b4521275d8f7da7f1
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-07 03:40:50 +0600

    updated InvoiceConfiguration

--- VaatcoBMS.Infrastructure/Persistence/InvoiceConfiguration.cs
+++ VaatcoBMS.Infrastructure/Persistence/InvoiceConfiguration.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using VaatcoBMS.Domain.Entities;
 
@@ -16,6 +15,9 @@ public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
 		b.Property(x => x.VAT).HasPrecision(18, 2); 
 		b.Property(x => x.TotalAmount).HasPrecision(18, 2); 
 		b.HasOne(x => x.Customer).WithMany(c => c.Invoices).HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict); 
-		b.HasOne(x => x.CreatedBy).WithMany(u => u.Invoices).HasForeignKey(x => x.CreatedByUserId).OnDelete(DeleteBehavior.Restrict); 
+		b.HasOne(i => i.CreatorUser)
+       .WithMany()
+       .HasForeignKey(i => i.CreatedBy) 
+       .OnDelete(DeleteBehavior.Restrict); // Advisable for creator IDs
 		b.HasMany(x => x.Items).WithOne(i => i.Invoice).HasForeignKey(i => i.InvoiceId).OnDelete(DeleteBehavior.Cascade); }
 }
