d9e5befa639d83ed2416a43969f4bf5f480d35a1
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-07 03:39:30 +0600

    added IINvoiceService interfaces

---
+++ VaatcoBMS.Application/Interfaces/IInvoiceService.cs
@@ -0,0 +1,14 @@
+﻿
+using VaatcoBMS.Application.DTOs;
+using VaatcoBMS.Domain.Enums;
+
+namespace VaatcoBMS.Application.Interfaces;
+
+public interface IInvoiceService
+{
+    Task<InvoiceDto> CreateAsync(InvoiceDto dto, int userId);
+    Task<InvoiceDto> GetByIdAsync(int id);
+    Task<IEnumerable<InvoiceDto>> GetAllAsync(int page, int pageSize);
+    Task UpdateStatusAsync(int id, InvoiceStatus status);
+    Task<bool> DeleteAsync(int id);
+}
