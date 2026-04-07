3e80ebd5a71adc8bd23f6215b674f069c36f1dbc
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-08 00:56:25 +0600

    updated InvoiceItem and User Dto

--- VaatcoBMS.Application/DTOs/InvoiceItem/UpdateInvoiceItemDto.cs
+++ VaatcoBMS.Application/DTOs/InvoiceItem/UpdateInvoiceItemDto.cs
@@ -4,7 +4,8 @@ public class UpdateInvoiceItemDto
 {
     public int Id { get; set; }
     public string Description { get; set; } = string.Empty;
-    public decimal Quantity { get; set; }
-    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+	public int BonusQuantity { get; set; }
+	public decimal UnitPrice { get; set; }
     public decimal Discount { get; set; }
 }
