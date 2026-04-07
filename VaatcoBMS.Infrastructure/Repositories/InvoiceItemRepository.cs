65d336e70e80e6a41d15dc5132be50b2fb41d3cb
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-08 01:02:22 +0600

    Added InvoiceItemRepository

---
+++ VaatcoBMS.Infrastructure/Repositories/InvoiceItemRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaatcoBMS.Domain.Entities;
+using VaatcoBMS.Domain.Interfaces;
+using VaatcoBMS.Infrastructure.Persistence;
+
+namespace VaatcoBMS.Infrastructure.Repositories;
+
+public class InvoiceItemRepository(AppDbContext context) : Repository<InvoiceItem>(context), IInvoiceItemRepository
+{
+	public async Task<IEnumerable<InvoiceItem>> GetByInvoiceIdAsync(int invoiceId)
+    {
+        return await _set.Where(x => x.InvoiceId == invoiceId).ToListAsync();
+    }
+
+    public async Task DeleteByIdAsync(int id)
+    {
+        var item = await GetByIdAsync(id);
+        if (item != null)
+        {
+            Delete(item);
+        }
+    }
+
+	public async Task<InvoiceItem> GetByIdAsync(int id)
+	{
+    return await _set.FindAsync(id);
+	}
+}
