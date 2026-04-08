c1ab4fc112ae2712a7335ef31b545ee9c58caaad
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-09 03:25:13 +0600

    updated ProductDtoValidator

--- VaatcoBMS.Application/DTOs/Product/UpdateProductDtoValidator.cs
+++ VaatcoBMS.Application/DTOs/Product/UpdateProductDtoValidator.cs
@@ -1,5 +1,16 @@
-﻿namespace VaatcoBMS.Application.DTOs.Product;
+﻿using FluentValidation;
 
-public class UpdateProductDtoValidator : CreateProductDtoValidator
+namespace VaatcoBMS.Application.DTOs.Product;
+
+public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
 {
+	public UpdateProductDtoValidator()
+	{
+		RuleFor(x => x.Id).GreaterThan(0);
+		RuleFor(x => x.ProductCode).NotEmpty().MaximumLength(30);
+		RuleFor(x => x.ProductName).NotEmpty().MaximumLength(200);
+		RuleFor(x => x.PackSize).NotEmpty().MaximumLength(50);
+		RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+		RuleFor(x => x.StockQuantity).GreaterThanOrEqualTo(0);
+	}
 }
