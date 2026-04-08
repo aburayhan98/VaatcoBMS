bdc93f3560b1146bdd1d8d197d8211df613c0a9b
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-09 02:37:05 +0600

    updated UnitOfWork Repositories

--- VaatcoBMS.Infrastructure/Repositories/UnitOfWork.cs
+++ VaatcoBMS.Infrastructure/Repositories/UnitOfWork.cs
@@ -3,25 +3,21 @@ using VaatcoBMS.Infrastructure.Persistence;
 
 namespace VaatcoBMS.Infrastructure.Repositories;
 
-public class UnitOfWork : IUnitOfWork
+public class UnitOfWork(AppDbContext ctx,
+	IInvoiceRepository invoices,
+	ICustomerRepository customers,
+	IProductRepository products,
+	IUserRepository users,
+	IInvoiceItemRepository invoiceItems) : IUnitOfWork
 {
-	private readonly AppDbContext _ctx;
-	public IInvoiceRepository Invoices { get; }
-	public ICustomerRepository Customers { get; }
-	public IProductRepository Products { get; }
-	public IUserRepository Users { get; }
-	public UnitOfWork(AppDbContext ctx, 
-		IInvoiceRepository invoices, 
-		ICustomerRepository customers, 
-		IProductRepository products,
-		IUserRepository users)
-	{
-		_ctx = ctx;
-		Invoices = invoices;
-		Customers = customers;
-		Products = products;
-		Users = users;
-	}
+	private readonly AppDbContext _ctx = ctx;
+	public IInvoiceRepository Invoices { get; } = invoices;
+	public ICustomerRepository Customers { get; } = customers;
+	public IProductRepository Products { get; } = products;
+	public IUserRepository Users { get; } = users;
+
+	public IInvoiceItemRepository InvoiceItems { get; } = invoiceItems;
+
 	public async Task SaveAsync()
 	{
 		await _ctx.SaveChangesAsync();
