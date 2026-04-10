bbd25d384ebea57dc4ca8072589f15629fc11fee
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-10 21:47:16 +0600

    added RequestloginMiddleware

---
+++ VaatcoBMS.Web/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace VaatcoBMS.Web.Middleware;
+
+public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+{
+	public async Task InvokeAsync(HttpContext ctx)
+	{
+		var sw = Stopwatch.StartNew();
+		await next(ctx);
+		sw.Stop();
+
+		logger.LogInformation(
+			"{Method} {Path}{Query} => {StatusCode} in {Ms}ms",
+			ctx.Request.Method,
+			ctx.Request.Path,
+			ctx.Request.QueryString,
+			ctx.Response.StatusCode,
+			sw.ElapsedMilliseconds);
+	}
+}
