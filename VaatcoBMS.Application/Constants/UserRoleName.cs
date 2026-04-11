7dbb88d5931c45c2b4bfa293a9a8618a8f94d45f
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-11 16:08:27 +0600

    updated UserRole

--- VaatcoBMS.Application/Constants/UserRoleName.cs
+++ VaatcoBMS.Application/Constants/UserRoleName.cs
@@ -3,5 +3,8 @@
 public static class UserRoleName
 {
 	public const string Admin = "Admin";
-	public const string User = "User";
+	public const string Employee = "Employee";
+	public const string MarketingOfficer = "MarketingOfficer";
+	public const string Distributor = "Distributor";
+	public const string SuperAdmin = "SuperAdmin";
 }
