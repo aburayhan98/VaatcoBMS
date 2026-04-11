eaf757953086e0710ba1a897e69e511fc689adc8
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-11 23:21:24 +0600

    added dbmigration

---
+++ VaatcoBMS.Infrastructure/Migrations/20260411102035_AddUserAndInvoiceItemProperty.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VaatcoBMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndInvoiceItemProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LowStockThreshold",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockStatus",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PackSize",
+                table: "InvoiceItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCode",
+                table: "InvoiceItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "InvoiceItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerCode",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LowStockThreshold",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockStatus",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "PackSize",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductCode",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerCode",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Customers");
+        }
+    }
+}
