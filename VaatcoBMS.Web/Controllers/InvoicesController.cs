4b518e0f3259bf0df8b0f0915ea5b520a4a0e6f4
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 03:01:11 +0600

    added InvoicesController

---
+++ VaatcoBMS.Web/Controllers/InvoicesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VaatcoBMS.Application.Interfaces;
+
+namespace VaatcoBMS.Web.Controllers;
+
+[Authorize]
+[Route("Invoices")]
+public class InvoicesController(
+		IInvoiceService invoiceService,
+		ICustomerService customerService,
+		IProductService productService) : Controller
+{
+	private readonly IInvoiceService _invoiceService = invoiceService;
+	private readonly ICustomerService _customerService = customerService;
+	private readonly IProductService _productService = productService;
+
+	[HttpGet("")]
+	public async Task<IActionResult> Index()
+	{
+		var invoices = await _invoiceService.GetAllAsync(1, 200);
+		return View(invoices);
+	}
+
+	[HttpGet("Create")]
+	public IActionResult Create() => View();
+
+	[HttpGet("{id:int}")]
+	public async Task<IActionResult> Detail(int id)
+	{
+		var invoice = await _invoiceService.GetByIdAsync(id);
+		if (invoice == null) return NotFound();
+		return View(invoice);
+	}
+}
