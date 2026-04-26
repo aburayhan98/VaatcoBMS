5c9abbe15417f690d7e67ce672e300384ce1e72e
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 02:59:23 +0600

    updated UnitOfWork

--- VaatcoBMS.Infrastructure/Repositories/UnitOfWork.cs
+++ VaatcoBMS.Infrastructure/Repositories/UnitOfWork.cs
@@ -3,29 +3,25 @@ using VaatcoBMS.Infrastructure.Persistence;
 
 namespace VaatcoBMS.Infrastructure.Repositories;
 
-public class UnitOfWork(AppDbContext ctx,
-	IInvoiceRepository invoices,
-	ICustomerRepository customers,
-	IProductRepository products,
-	IUserRepository users,
-	IInvoiceItemRepository invoiceItems) : IUnitOfWork
+public class UnitOfWork(
+		AppDbContext ctx,
+		IInvoiceRepository invoices,
+		ICustomerRepository customers,
+		IProductRepository products,
+		IUserRepository users,
+		IInvoiceItemRepository invoiceItems,
+		IPaymentRepository payments) : IUnitOfWork
 {
 	private readonly AppDbContext _ctx = ctx;
 	public IInvoiceRepository Invoices { get; } = invoices;
 	public ICustomerRepository Customers { get; } = customers;
 	public IProductRepository Products { get; } = products;
 	public IUserRepository Users { get; } = users;
-
 	public IInvoiceItemRepository InvoiceItems { get; } = invoiceItems;
+	public IPaymentRepository Payments { get; } = payments;
 
-	public async Task SaveAsync()
-	{
-		await _ctx.SaveChangesAsync();
-	}
-	public async Task<int> SaveChangesAsync()
-	{
-		return await _ctx.SaveChangesAsync();
-	}
+	public async Task<int> SaveChangesAsync() => await _ctx.SaveChangesAsync();
+
 	public void Dispose()
 	{
 		_ctx.Dispose();
