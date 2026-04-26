f5f52b79ea6df1af706826741ed9e1949d668a05
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 02:57:23 +0600

    updated IInvioceRepository

--- VaatcoBMS.Domain/Interfaces/IInvoiceRepository.cs
+++ VaatcoBMS.Domain/Interfaces/IInvoiceRepository.cs
@@ -7,8 +7,10 @@ namespace VaatcoBMS.Domain.Interfaces;
 public interface IInvoiceRepository : IRepository<Invoice>
 {
 	Task<Invoice> GetWithDetailsAsync(int id);
+	Task<IEnumerable<Invoice>> GetPagedWithDetailsAsync(int page, int pageSize);
 	Task<IEnumerable<Invoice>> GetByCustomerAsync(int customerId);
 	Task<IEnumerable<Invoice>> GetByUserAsync(int userId); 
 	Task<string> GetNextInvoiceNumberAsync(); 
 	Task<IEnumerable<Invoice>> GetByStatusAsync(InvoiceStatus status);
+	Task<(decimal TotalRevenue, decimal TotalPaid, int TotalCount, int DraftCount, int ApprovedCount, int PaidCount, int CancelledCount)> GetStatsAsync();
 }
