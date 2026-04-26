eff0384463ea793c021d0ea0fbde501a9c2297d5
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 02:58:53 +0600

    added PaymentRepository

---
+++ VaatcoBMS.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaatcoBMS.Domain.Entities;
+using VaatcoBMS.Domain.Interfaces;
+using VaatcoBMS.Infrastructure.Persistence;
+
+namespace VaatcoBMS.Infrastructure.Repositories;
+
+public class PaymentRepository(AppDbContext ctx) : Repository<Payment>(ctx), IPaymentRepository
+{
+	public async Task<IEnumerable<Payment>> GetByInvoiceIdAsync(int invoiceId)
+	{
+		return await _set
+				.Where(p => p.InvoiceId == invoiceId)
+				.OrderByDescending(p => p.PaymentDate)
+				.ToListAsync();
+	}
+
+	public async Task<decimal> GetTotalPaidAsync(int invoiceId)
+	{
+		return await _set
+				.Where(p => p.InvoiceId == invoiceId)
+				.SumAsync(p => (decimal?)p.Amount) ?? 0m;
+	}
+}
