10c37417aea7410dc88ba11893ed48328f1bab93
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 02:54:34 +0600

    added IPaymentService

---
+++ VaatcoBMS.Application/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using VaatcoBMS.Application.DTOs.Payment;
+
+namespace VaatcoBMS.Application.Interfaces;
+
+public interface IPaymentService
+{
+	Task<IEnumerable<PaymentDto>> GetByInvoiceAsync(int invoiceId);
+	Task<PaymentDto> CreateAsync(CreatePaymentDto dto);
+	Task DeleteAsync(int id);
+}
