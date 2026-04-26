7e05996d0fbbab242b06587917c9ce66ccaae112
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-27 02:55:54 +0600

    updated InvoiceEntity

--- VaatcoBMS.Domain/Entities/Invoice.cs
+++ VaatcoBMS.Domain/Entities/Invoice.cs
@@ -8,8 +8,11 @@ public class Invoice : BaseEntityWithAudit
 	public int CustomerId { get; set; }
 
 	public DateTime InvoiceDate { get; set; }
+	public DateTime? DueDate { get; set; } //new
+	public string? ReferenceNumber { get; set; } //new
 	public decimal Subtotal { get; set; }
 	public decimal Discount { get; set; }
+	public decimal TaxRate { get; set; } // percentage e.g. 15 = 15%
 	public decimal VAT { get; set; }
 	public decimal TotalAmount { get; set; }
 	public InvoiceStatus Status { get; set; } = InvoiceStatus.Draft; 
@@ -20,4 +23,5 @@ public class Invoice : BaseEntityWithAudit
 	public User CreatorUser { get; set; } = null!; 
 	
 	public ICollection<InvoiceItem> Items { get; set; } = new List<InvoiceItem>();
+	public ICollection<Payment> Payments { get; set; } = new List<Payment>();
 }
