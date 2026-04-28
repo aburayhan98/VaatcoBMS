a708f91224c9c45cad3951f2f63bafeebcd49524
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-04-28 22:01:03 +0600

    added SQL for AddProductIndex

---
+++ AddProductIndex.sql
@@ -0,0 +1,12 @@
+﻿
+
+CREATE INDEX IX_Products_Name_IsActive
+    ON dbo.Products (IsActive, Name)
+    INCLUDE (Code, PackSize, Price, StockQuantity, StockStatus);
+
+CREATE INDEX IX_Products_Code
+    ON dbo.Products (Code)
+    INCLUDE (Name, PackSize, Price, StockQuantity, StockStatus, IsActive);
+
+    /*SELECT name, type_desc FROM sys.indexes WHERE object_id = OBJECT_ID('dbo.Products');
+EXEC sp_helpindex 'dbo.Products';*/
